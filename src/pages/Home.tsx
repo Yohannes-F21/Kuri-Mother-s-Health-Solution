bec4a2976b167ee7aa240a2dd0d96be0dc547f9c
Yohannes <yohannesfantahun88@gmail.com>
2025-02-06 11:50:27 +0300

    changing some contents and fixing the carousel

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -81,9 +81,7 @@ const Home = () => {
           </div>
         </div>
       </section>
-      <section className="mb-10">
-        <PartnerCarousel />
-      </section>
+      <section className="mb-10"></section>
       <section className="py-16 px-4 sm:px-6 lg:px-8">
         <div className="max-w-7xl mx-auto">
           <h2 className="text-3xl font-semibold text-center mb-12">
